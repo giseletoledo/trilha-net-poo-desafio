8a5915ee5e98e8be8cb1a89a9256aa4162e16cd6
gisele <gisesonia@gmail.com>
2024-05-30 21:55:46 -0300

    criando as propriedades do diagrama samrtphone

--- Models/Smartphone.cs
+++ Models/Smartphone.cs
@@ -3,7 +3,10 @@ namespace DesafioPOO.Models
     public abstract class Smartphone
     {
         public string Numero { get; set; }
-        // TODO: Implementar as propriedades faltantes de acordo com o diagrama
+        public string Modelo { get; set; }
+        public string IMEI { get; set;}
+        public int Memoria { get; set;}
+
 
         public Smartphone(string numero)
         {
